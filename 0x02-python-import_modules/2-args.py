21350e435d79ab31df3e27d3045f6e7dc32419a2
Cemordi2 <emordichukwuma@gmail.com>
2022-10-01 05:26:47 -0700

    m

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -12,6 +12,6 @@ if __name__ == "__main__":
 
     if i >= 1:
         i = 0;
-        for (i = 1; sys.arg; i++) 
+        for (i = 1; sys.arg[i]; i++) 
             print("{}: {}".format(i, sys.argv[i]))
 
