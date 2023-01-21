5e58672b22f2585fe14ee586b43a87e5418d4ac8
Cemordi21 <emordichukwuma@gmail.com>
2023-01-21 16:20:44 +0000

    update code

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -9,12 +9,9 @@ import urllib.parse
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    value = {"email": "sys.argv[2]"}
-    """ encoding the data fromdictionary to byte object"""
+    value = {'email': sys.argv[2]}
     encoding_value = urllib.parse.urlencode(value)
-    """convert to byte"""
     value_coded = encoding_value.encode("ascii")
-
-        
+      
     with urllib.request.urlopen(url, data = value_coded) as response:
         print(response.read().decode("utf-8"))
