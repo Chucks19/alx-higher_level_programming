075f89d087f4149863cf060e19c3bddba9da30a1
Cemordi21 <emordichukwuma@gmail.com>
2023-01-21 15:44:54 +0000

    update code

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -9,6 +9,6 @@ import urllib.request
 if __name__ == "__main__":
     url = sys.argv[1]
 
-    request = urllib.request.Request(url)
+    request = url
     with urllib.request.urlopen(request) as response:
-        print(dict(response.headers).get("X-Request-Id"))
+        print(dict(response.headers["X-Request-Id"]))
